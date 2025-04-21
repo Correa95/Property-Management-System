37eadbaa2f47aec62bdfb0956e58da44507eecd7
Correa95 <mathewcorrea19@gmail.com>
2025-04-21 09:53:11 -0400

    change the styling to reguler css

--- client-side/src/Components/Analysis.jsx
+++ client-side/src/Components/Analysis.jsx
@@ -1,22 +1,18 @@
-import styles from "./Analysis.module.css";
-import property from "../apartmentBuilding/property.js";
+import "./Analysis.css";
 function Analysis() {
-  // console.log(property.units);
-
   return (
-    <section className={styles.statsContainer}>
-      <div className={styles.stats}>
-        {property.units.map((unit) => console.log(unit.length))}
+    <section className="statsContainer">
+      <div className="stats">
         <h1>Vacancy Rate</h1>
         <amount>100%</amount>
         <small>As of Last Month</small>
       </div>
-      <div className={styles.stats}>
+      <div className="stats">
         <h1>Total Rent Collected</h1>
         <amount>$100000</amount>
         <small>As of Last Month</small>
       </div>
-      <div className={styles.stats}>
+      <div className="stats">
         <h1>Total Expenses</h1>
         <amount>$250000</amount>
         <small>As of Last Month</small>
