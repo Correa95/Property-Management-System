16ac771d5389f328dc55089fe8aab29d532e1aa2
Correa95 <mathewcorrea19@gmail.com>
2025-04-21 08:54:35 -0400

    commented some code

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,11 +1,11 @@
-# from rest_framework.decorators import api_view, permission_classes
-# from rest_framework.response import Response
-# from rest_framework import status
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# from django.shortcuts import redirect
-# from api.models import Apartment, ApartmentComplex, Tenant, Lease, Payment
-# from .serializers import ApartmentSerializer, ApartmentComplexSerializer, TenantSerializer, LeaseSerializer, MaintenanceRequest, PaymentSerializer 
+from django.shortcuts import redirect
+from api.models import Apartment, ApartmentComplex, Tenant, Lease, Payment
+from .serializers import ApartmentSerializer, ApartmentComplexSerializer, TenantSerializer, LeaseSerializer, MaintenanceRequest, PaymentSerializer 
 
 # Create your views here.
 
