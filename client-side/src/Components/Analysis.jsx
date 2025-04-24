4bc6fa75cc6ceaaa4c8d6a7fca89daa2fea3f5bf
Correa95 <mathewcorrea19@gmail.com>
2025-04-24 07:41:52 -0400

    styline the app structure

--- client-side/src/Components/Analysis.jsx
+++ client-side/src/Components/Analysis.jsx
@@ -3,19 +3,19 @@ function Analysis() {
   return (
     <section className="statsContainer">
       <div className="stats">
-        <h1>Vacancy Rate</h1>
+        <h1 className="titleHeader">Vacancy Rate</h1>
         <amount className="dollarAmount">100%</amount>
         <br />
         <small>As of Last Month</small>
       </div>
       <div className="stats">
-        <h1>Total Rent Collected</h1>
+        <h1 className="titleHeader">Total Rent Collected</h1>
         <amount className="dollarAmount">$100000</amount>
         <br />
         <small>As of Last Month</small>
       </div>
       <div className="stats">
-        <h1>Total Expenses</h1>
+        <h1 className="titleHeader">Total Expenses</h1>
         <amount className="dollarAmount">$250000</amount>
         <br />
         <small className="timeUpdate">As of Last Month</small>
