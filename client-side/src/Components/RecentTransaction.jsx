e51b6f42deff208a71fb9efcdcd6241e3f557c03
Correa95 <mathewcorrea19@gmail.com>
2025-05-11 12:58:29 -0400

    working on recent transactions

--- client-side/src/Components/RecentTransaction.jsx
+++ client-side/src/Components/RecentTransaction.jsx
@@ -1,10 +1,42 @@
 function RecentTransaction() {
   return (
-    <div className="recentTransaction">
-      <h1>Recent Transaction</h1>
-      <h2>Record bill</h2>
-      <h3>export</h3>
-    </div>
+    <>
+      <div className="recentTransaction">
+        <div className="top">
+          <h1>Recent Transaction</h1>
+          <h2>Record bill</h2>
+          <h3>export</h3>
+        </div>
+      </div>
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>Ref No</th>
+              <th>Customer</th>
+              <th>Due Date</th>
+              <th>Status</th>
+              <th>Memo</th>
+              <th>Amount</th>
+              <th>Action</th>
+              <th>Header 8</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
