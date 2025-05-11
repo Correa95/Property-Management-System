ae37798920ab4be9c29fd48580e05d6bce90827f
Correa95 <mathewcorrea19@gmail.com>
2025-05-11 13:02:07 -0400

    working on recent transactions

--- client-side/src/Components/Analysis.jsx
+++ client-side/src/Components/Analysis.jsx
@@ -3,14 +3,20 @@ function Analysis() {
   return (
     <section className="statsContainer">
       <div className="stats">
-        <h1 className="titleHeader">Vacancy Rate</h1>
+        <h1 className="titleHeader">Rent Received</h1>
         <amount className="dollarAmount">100%</amount>
         <br />
         <small className="timeStamp">As of Last Month</small>
       </div>
       <div className="stats">
-        <h1 className="titleHeader">Total Rent Collected</h1>
+        <h1 className="titleHeader">Upcoming Payments</h1>
         <amount className="dollarAmount">$100000</amount>
+        <br />
+        <small className="timeStamp">As of Last Month</small>
+      </div>
+      <div className="stats">
+        <h1 className="titleHeader">Rent Overdue</h1>
+        <amount className="dollarAmount">$250000</amount>
         <br />
         <small className="timeStamp">As of Last Month</small>
       </div>
