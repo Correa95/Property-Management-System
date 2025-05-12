05117ca87c7ed3e9e95926d8adf3991794a3474e
Correa95 <mathewcorrea19@gmail.com>
2025-05-12 10:40:27 -0400

    styling the side bar

--- client-side/src/Components/Overview.jsx
+++ client-side/src/Components/Overview.jsx
@@ -1,13 +1,13 @@
-// import Analysis from "./Analysis";
-// import Charts from "./Charts";
-// import RecentTransactions from "./RecentTransactions";
+import Analysis from "./Analysis";
+import Charts from "./Charts";
+import RecentTransactions from "./RecentTransactions";
 import "./OverView.css";
 function OverView() {
   return (
     <div className="overViewContainer">
-      {/* <Analysis />
+      <Analysis />
       <Charts />
-      <RecentTransactions /> */}
+      <RecentTransactions />
 
       <h1>MATHEW</h1>
     </div>
