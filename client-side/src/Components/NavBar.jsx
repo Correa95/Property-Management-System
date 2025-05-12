ac706e11b0f71df4e90748c9b13eb54911bfea22
Correa95 <mathewcorrea19@gmail.com>
2025-05-12 11:09:18 -0400

    styling the side bar

--- client-side/src/Components/NavBar.jsx
+++ client-side/src/Components/NavBar.jsx
@@ -1,17 +1,25 @@
 import "./NavBar.css";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { PiEnvelopeSimpleLight } from "react-icons/pi";
 function NavBar() {
   return (
-    <nav className="navBar">
-      <ul>
-        <li>NOTIFICATION</li>
-        <li>NOTIFICATION</li>
-        <li>NOTIFICATION</li>
-        <li>NOTIFICATION</li>
-        <li>NOTIFICATION</li>
-        <li>NOTIFICATION</li>
-        <li>NOTIFICATION</li>
-        <li>NOTIFICATION</li>
-      </ul>
+    <nav className="navBarContainer">
+      <div className="navBar">
+        <div className="search">
+          <input type="text" name="" id="" />
+        </div>
+        <div className="notification">
+          <button>
+            <PiEnvelopeSimpleLight />
+          </button>
+          <button>
+            <IoMdNotificationsOutline />
+          </button>
+        </div>
+        <div className="profile">
+          <img src="" alt="" />
+        </div>
+      </div>
     </nav>
   );
 }
