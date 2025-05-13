b5d01fd9da9af166130b5e26f011d210f88c8956
Correa95 <mathewcorrea19@gmail.com>
2025-05-12 21:38:33 -0400

    styling the overview

--- client-side/src/Components/NewTenant.jsx
+++ client-side/src/Components/NewTenant.jsx
@@ -4,55 +4,57 @@ function NewTenant() {
   return (
     <div className="newTenantContainer">
       <h1 className="addTenantHeader">ADD NEW TENANT</h1>
-      <form className="tenantDetail">
-        <h1>Tenant Detail</h1>
-        <label>
-          First Name:
-          <input type="text" />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" />
-        </label>
-        <label>
-          Phone Number:
-          <input type="text" />
-        </label>
-        <label>
-          Email :
-          <input type="email" />
-        </label>
-        <label>
-          unit Number:
-          <input type="text" />
-        </label>
-        <label>
-          Building Number :
-          <input type="text" />
-        </label>
-      </form>
+      <div className="formContainer">
+        <form className="newTenantForm">
+          <h1 className="">Tenant Detail</h1>
+          <label>
+            First Name:
+            <input type="text" />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" />
+          </label>
+          <label>
+            Phone Number:
+            <input type="text" />
+          </label>
+          <label>
+            Email :
+            <input type="email" />
+          </label>
+          <label>
+            unit Number:
+            <input type="text" />
+          </label>
+          <label>
+            Building Number :
+            <input type="text" />
+          </label>
+        </form>
 
-      <form className="tenantPayment">
-        <h1>Payment Details</h1>
-        <label>
+        {/* <form className="tenantPayment">
+          <h1>Payment Details</h1>
           <label>
-            Deposit Amount:
-            <input type="text" />
+            <label>
+              Deposit Amount:
+              <input type="text" />
+            </label>
+            <label>
+              Monthly Rent:
+              <input type="text" />
+            </label>
+            <label>
+              Start Date:
+              <input type="date" />
+            </label>
+            <label>
+              End Date:
+              <input type="date" />
+            </label>
           </label>
-          <label>
-            Monthly Rent:
-            <input type="text" />
-          </label>
-          <label>
-            Start Date:
-            <input type="date" />
-          </label>
-          <label>
-            End Date:
-            <input type="date" />
-          </label>
-        </label>
-      </form>
+        </form> */}
+      </div>
     </div>
   );
 }
