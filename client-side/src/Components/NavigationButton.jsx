e4865b490a67b3f0b02177814eaa5c034e72636a
Correa95 <mathewcorrea19@gmail.com>
2025-05-13 12:04:51 -0400

    create the new tenant component

--- client-side/src/Components/NavigationButton.jsx
+++ client-side/src/Components/NavigationButton.jsx
@@ -1,10 +1,26 @@
+import { Link } from "react-router-dom";
 import "./NavigationButton.css";
+import TenantInfoForm from "./Form/TenantInfoForm";
+import LeaseForm from "./Form/LeaseForm";
+import InitialDepositForm from "./Form/InitialDepositForm";
 function NavigationButton() {
   return (
     <div className="NavigationButtonContainer">
-      <button className="btn">Add Tenant</button>
-      <button className="btn">Add Lease</button>
-      <button className="btn">Initial Deposit</button>
+      <button className="btn">
+        <Link to="/" element={<TenantInfoForm />}>
+          Add Tenant
+        </Link>
+      </button>
+      <button className="btn">
+        <Link to="/lease" element={<LeaseForm />}>
+          Add Lease
+        </Link>
+      </button>
+      <button className="btn">
+        <Link to="/InitialDepositForm" element={<InitialDepositForm />}>
+          Initial Deposit
+        </Link>
+      </button>
     </div>
   );
 }
