ebd7a1563d1a1b97f042e1bc0a9d835c8529445d
Correa95 <mathewcorrea19@gmail.com>
2025-05-12 20:35:33 -0400

    styling the overview

--- client-side/src/Components/Units.jsx
+++ client-side/src/Components/Units.jsx
@@ -1,6 +1,36 @@
 import "./Units.css";
 function Units() {
-  return <div className="unitesContainer"></div>;
+  return (
+    <div className="unitesContainer">
+      <h1 className="unitsHeader">All Units</h1>
+      <div className="unitsTable">
+        <table>
+          <thead>
+            <tr>
+              <th>Building</th>
+              <th>Building Number</th>
+              <th>Unit Number</th>
+              <th>Num Bedrooms</th>
+              <th>Sq Ft</th>
+              <th>Rent Amount</th>
+              <th>Availability</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Units;
