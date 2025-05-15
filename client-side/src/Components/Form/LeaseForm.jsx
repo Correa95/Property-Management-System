abf63bd475c377250cfd3cc89c4e02ab61fa80e4
Correa95 <mathewcorrea19@gmail.com>
2025-05-15 11:49:34 -0400

    working on the new tenanat form

--- client-side/src/Components/Form/LeaseForm.jsx
+++ client-side/src/Components/Form/LeaseForm.jsx
@@ -1,5 +1,37 @@
+import "./Form/LeaseForm.css";
 function Lease() {
-  return <div>CREATE A LEASE FORM</div>;
+  return (
+    <div className="leaseFormContainer">
+      <form className="leaseForm">
+        <div className="apartmentInfo">
+          <label>
+            Apartment:
+            <input type="text" />
+          </label>
+          <label>
+            Tenant:
+            <input type="text" />
+          </label>
+        </div>
+        <div className="dates">
+          <label>
+            Start Date
+            <input type="date" />
+          </label>
+          <label>
+            End Date
+            <input type="date" />
+          </label>
+        </div>
+        <div className="amounts">
+          <label>
+            MOnthly Rent
+            <input type="number" />
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Lease;
