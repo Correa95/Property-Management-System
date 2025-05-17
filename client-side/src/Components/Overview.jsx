03a2fdc7cf913fd4d3a2e3289e05f1c4d4ddcb12
Correa95 <mathewcorrea19@gmail.com>
2025-05-17 10:44:43 -0400

    adjusting the width of the overview component

--- client-side/src/Components/Overview.jsx
+++ client-side/src/Components/Overview.jsx
@@ -1,17 +1,14 @@
 import Analysis from "./Analysis";
 import Charts from "./Charts";
 import RecentTransaction from "./RecentTransaction";
-import NaveBar from "./NavBar";
+import NavBar from "./NavBar";
 import "./OverView.css";
 function OverView() {
   return (
     <div className="overViewContainer">
-      <NaveBar />
+      <NavBar />
       <h1 className="overViewHeader">Overview</h1>
       <div className="container">
-        <div className="box">
-          <Analysis />
-        </div>
         <div className="box">
           <Analysis />
         </div>
