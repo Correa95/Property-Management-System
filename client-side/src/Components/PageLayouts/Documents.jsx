3666ab860d418d709c9f61de4f6c45e335d4ca05
Correa95 <mathewcorrea19@gmail.com>
2025-05-24 10:12:06 -0400

    refactoring the application

--- client-side/src/Components/PageLayouts/Documents.jsx
+++ client-side/src/Components/PageLayouts/Documents.jsx
@@ -3,18 +3,17 @@ import "./Documents.css";
 function Documents() {
   return (
     <div className="documentsContainer">
-      <div className="type">
-        <h1 className="title">Documents</h1>
-        <ul className="btnNav">
-          <ol>All Documents</ol>
-          <ol>Receipts</ol>
-          <ol>Contracts</ol>
-          <ol>Other</ol>
-          <ol>Other</ol>
-        </ul>
+      {/* <div className="type"> */}
+      <div className="btnNav">
+        <h2 className="documentsTitle">Documents</h2>
+        <button>All Documents</button>
+        <button>Receipts</button>
+        <button>Contracts</button>
+        <button>Other</button>
       </div>
+
       <div className="upload"></div>
-      <h1>All Receipts</h1>
+      {/* <h1>All Receipts</h1> */}
     </div>
   );
 }
