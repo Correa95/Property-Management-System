65e997a6473e2ff647c30f117c75ec5ef83d2232
Correa95 <mathewcorrea19@gmail.com>
2025-05-24 23:20:17 -0400

    styling the unit component

--- client-side/src/Components/PageLayouts/Units.jsx
+++ client-side/src/Components/PageLayouts/Units.jsx
@@ -1,13 +1,13 @@
 import "./Units.css";
+
 function Units() {
   return (
-    <div className="unitesContainer">
+    <div className="unitsContainer">
       <h1 className="unitsHeader">All Units</h1>
       <div className="unitsTable">
         <table>
           <thead>
             <tr>
-              <th>Building</th>
               <th>Building Number</th>
               <th>Unit Number</th>
               <th>Num Bedrooms</th>
@@ -18,13 +18,12 @@ function Units() {
           </thead>
           <tbody>
             <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
+              <td>100</td>
+              <td>101</td>
+              <td>3</td>
+              <td>1100</td>
+              <td>2500</td>
+              <td>Active</td>
             </tr>
           </tbody>
         </table>
