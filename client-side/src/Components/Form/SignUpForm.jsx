ca3db419f659141372989261bf8fe3ca886617e1
Correa95 <mathewcorrea19@gmail.com>
2025-06-03 09:54:56 -0400

    refactoring the code in the sign up component

--- client-side/src/Components/Form/SignUpForm.jsx
+++ client-side/src/Components/Form/SignUpForm.jsx
@@ -2,21 +2,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./SignUpForm.css";
 
-// function getCookie(name) {
-//   let cookieValue = null;
-//   if (document.cookie && document.cookie !== "") {
-//     const cookies = document.cookie.split(";");
-//     for (let i = 0; i < cookies.length; i++) {
-//       const cookie = cookies[i].trim();
-//       if (cookie.substring(0, name.length + 1) === name + "=") {
-//         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-//         break;
-//       }
-//     }
-//   }
-//   return cookieValue;
-// }
-
 function SignUpForm() {
   const navigate = useNavigate();
   const [firstName, setFirstName] = useState("");
