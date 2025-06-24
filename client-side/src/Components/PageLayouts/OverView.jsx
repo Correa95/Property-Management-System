adaa607334d90761d4ae1b58eaf706cc5493153d
Correa95 <mathewcorrea19@gmail.com>
2025-06-24 10:24:07 -0400

    styling the monthly statement component

--- client-side/src/Components/PageLayouts/OverView.jsx
+++ client-side/src/Components/PageLayouts/OverView.jsx
@@ -1,29 +1,29 @@
-import Analysis from "../InnerComponents/Analysis";
-import Charts from "../InnerComponents/Charts";
-import RecentTransaction from "../InnerComponents/RecentTransaction";
-import NavBar from "../NavigationBars/NavBar";
-import "./OverView.css";
-function OverView() {
-  return (
-    <div className="overViewContainer">
-      <div className="navComponent">
-        <NavBar />
-      </div>
-      <h1 className="overViewHeader">Overview</h1>
-      <div className="container">
-        <div className="box">
-          <Analysis />
-        </div>
+// import Analysis from "../InnerComponents/Analysis";
+// import Charts from "../InnerComponents/Charts";
+// import RecentTransaction from "../InnerComponents/RecentTransaction";
+// import NavBar from "../NavigationBars/NavBar";
+// import "./OverView.css";
+// function OverView() {
+//   return (
+//     <div className="overViewContainer">
+//       <div className="navComponent">
+//         <NavBar />
+//       </div>
+//       <h1 className="overViewHeader">Overview</h1>
+//       <div className="container">
+//         <div className="box">
+//           <Analysis />
+//         </div>
 
-        <div className="box">
-          <Charts />
-        </div>
-        <div className="box">
-          <RecentTransaction />
-        </div>
-      </div>
-    </div>
-  );
-}
+//         <div className="box">
+//           <Charts />
+//         </div>
+//         <div className="box">
+//           <RecentTransaction />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default OverView;
+// export default OverView;
