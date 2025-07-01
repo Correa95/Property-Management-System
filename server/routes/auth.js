837b678adda3900f6174e96272667272dc742dd3
Correa95 <mathewcorrea19@gmail.com>
2025-07-01 11:21:01 -0400

    preparing the server.js for deployment

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,15 @@
+// const express = require("express");
+// const router = express.Router();
+// // const { register } = require("../controller/authController");
+// const { register, login } = require("../controller/authController");
+// router.post("/register", register);
+// router.post("/login", login);
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
-// const { register } = require("../controller/authController");
-const { register, login } = require("../controller/authController");
+const { register, login } = require("../controllers/authController");
+
 router.post("/register", register);
 router.post("/login", login);
 
