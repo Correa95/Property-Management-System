f2f50b7befc869944c715e6b4a12a9ab5aa25537
Correa95 <mathewcorrea19@gmail.com>
2025-07-01 12:39:17 -0400

    preparing the server.js for deployment

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.use("/auth", require("./auth"));
 router.use("/user", require("./user"));
-router.use("/apartmentComplex", require("./apartmentComplex"));
+// router.use("/apartmentComplex", require("./apartmentComplex"));
 router.use("/building", require("./building"));
 router.use("/apartment", require("./apartment"));
 router.use("/tenant", require("./tenant"));
