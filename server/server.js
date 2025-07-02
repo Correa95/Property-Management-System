8974d381b0b4fc8f402309a7c72e6e452eaf285b
Correa95 <mathewcorrea19@gmail.com>
2025-07-02 12:37:55 -0400

    update the port for deployment

--- server/server.js
+++ server/server.js
@@ -5,24 +5,25 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
-// const PORT = 3000;
 const PORT = process.env.PORT || 3000;
-// const PORT = process.env.PORT || 8080;
+
+// CORS configuration
+const corsOptions = {
+  origin: [
+    "http://localhost:5173", // for local dev
+    "https://property-management-system-frontend-x6n0.onrender.com", // for production
+  ],
+  credentials: true,
+};
 
 app.use(express.json()); // ✅ needed to parse JSON bodies
 const controller = require("./routes");
 
-// CORS configuration
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-
+// CORS setup
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); // Handle preflight requests globally
 
 // Middleware setup
-app.use(express.json());
 app.use(morgan("dev"));
 
 // Routes
