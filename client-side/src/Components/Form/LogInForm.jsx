dedc8051bceaf24e32a1d0f36382a384b7302d0c
Correa95 <mathewcorrea19@gmail.com>
2025-07-02 11:06:55 -0400

    preparing the frontend for deployment

--- client-side/src/Components/Form/LogInForm.jsx
+++ client-side/src/Components/Form/LogInForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
-import "./LoginForm.css";
+import "./Login.css";
 
 function LoginForm() {
   const navigate = useNavigate();
