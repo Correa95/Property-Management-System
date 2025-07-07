5232223e526dd12f5d850dcec1f5ab9ec89a0f22
Correa95 <mathewcorrea19@gmail.com>
2025-07-07 11:40:04 -0400

    add the maintenance component

--- client-side/src/Components/PageLayouts/Maintenance.jsx
+++ client-side/src/Components/PageLayouts/Maintenance.jsx
@@ -3,22 +3,36 @@ import { useState } from "react";
 const requests = [
   {
     id: 1,
-    tenant: "John Doe",
-    date: "2025-07-01",
+    tenant: "Alice Johnson",
+    date: "2025-03-01",
     description:
       "Leaking faucet in the kitchen sink. It has been dripping continuously for the last few days.",
   },
   {
     id: 2,
-    tenant: "Jane Smith",
-    date: "2025-07-03",
+    tenant: "Victor Chang",
+    date: "2025-04-20",
     description:
       "The AC unit is not cooling properly. It blows warm air even after running for a while.",
   },
   {
     id: 3,
-    tenant: "Mike Johnson",
-    date: "2025-07-05",
+    tenant: "Grace Lee",
+    date: "2025-06-05",
+    description:
+      "Bathroom light keeps flickering and goes off randomly. Might be a wiring issue.",
+  },
+  {
+    id: 4,
+    tenant: "Emily Baker",
+    date: "2025-06-30",
+    description:
+      "Bathroom light keeps flickering and goes off randomly. Might be a wiring issue.",
+  },
+  {
+    id: 5,
+    tenant: "Henry Clark",
+    date: "2025-06-15",
     description:
       "Bathroom light keeps flickering and goes off randomly. Might be a wiring issue.",
   },
